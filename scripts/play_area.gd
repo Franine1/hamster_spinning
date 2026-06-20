a47022f34e2ec1df1972b0aed45d633468c98829
Franine1 <franen321divinenergy@gmail.com>
2026-06-20 16:30:47 -0400

    play area setup

---
+++ scripts/play_area.gd
@@ -0,0 +1,23 @@
+class_name PlayArea
+extends TileMapLayer
+
+
+@export var bounds: Rect2i = Rect2i(-20,-20,40,40)
+@onready var camera: Camera2D = %Camera2D
+
+
+func _ready() -> void:
+	reset()
+
+
+func reset() -> void:
+	clear()
+	var origin = bounds.position
+	for i in bounds.size.x:
+		for j in bounds.size.y:
+			set_cell(origin+Vector2i(i,j),0,Vector2i(0,0))
+	
+	var temp = Rect2(bounds)
+	temp.position *= 32.0
+	temp.size *= 32.0
+	camera.bounds = temp
---
+++ scripts/play_camera.gd
@@ -0,0 +1,27 @@
+extends Camera2D
+
+@export var speed = 900.0
+
+var bounds: Rect2 = Rect2(-1000,-1000,2000,2000)
+
+
+
+func _process(delta: float) -> void:
+	var movement := Input.get_vector("Move Left","Move Right","Move Up","Move Down")
+	
+	if movement:
+		var axis = (movement.max_axis_index())
+		#movement *= Vector2(1-axis,axis)
+		#movement = movement.normalized()
+		
+		position += movement * speed * delta
+	
+	var edges = Vector2(get_window().size)
+	
+	var temp = bounds
+	
+	
+	temp.position += (temp.size/2.0).min(edges/2.0)
+	temp.size -= (temp.size).min(edges)
+	
+	position = position.clamp(temp.position,temp.end)
