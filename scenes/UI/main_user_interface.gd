2a3f3718add8bcc09d9f3e7f85ece2d9eed832b3
Franine1 <franen321divinenergy@gmail.com>
2026-06-26 23:09:12 -0400

    animation managers complete

--- scenes/UI/main_user_interface.gd
+++ scenes/UI/main_user_interface.gd
@@ -9,7 +9,7 @@ func _process(_delta: float) -> void:
 	
 	%HP_value.text = str(snapped(game.get_HP(),0.1))
 	%money_value.text = str(snapped(game.get_money(),0.1))
-	%food_value.text = str(snapped(game.get_food(),0.1))
+	%food_value.text = str(snapped(game.get_food()+game.held_food(),0.1))
 	%HP_rate.text = str(snapped(game.get_HP_rate(),0.1)) + " / s"
 	%money_rate.text = str(snapped(game.get_money_rate(),0.1)) + " / s"
-	%food_rate.text = str(snapped(game.get_food_rate()+game.held_food(),0.1)) + " / s"
+	%food_rate.text = str(snapped(game.get_food_rate(),0.1)) + " / s"
--- scripts/animation_manager.gd
+++ scripts/animation_manager.gd
@@ -3,13 +3,32 @@ extends AnimatedSprite2D
 
 
 @export var delta_offset: float = 0.0
+@export var alt_animation: String = ""
 var auto_animate: bool = false
+var def_animation: String = ""
+
+
+func _ready() -> void:
+	def_animation = animation
 
 func _process(_delta: float) -> void:
 	var time: float = Time.get_unix_time_from_system()
 	if auto_animate:
 		game_animation(time)
 
+func alt_condition(input: bool) -> void:
+	if input and sprite_frames.has_animation(alt_animation):
+		swap_animation(alt_animation)
+	else:
+		swap_animation(def_animation)
+	
+	for child in get_children():
+		if child is AnimationManager:
+			child.alt_condition(input)
+
+func swap_animation(input: String) -> void:
+	if input != animation:
+		animation = input
 
 func game_animation(time: float, extra_speed: float = 1.0) -> void:
 	var my_time = time + delta_offset
@@ -25,8 +44,8 @@ func game_animation(time: float, extra_speed: float = 1.0) -> void:
 	
 
 
-func start() -> void:
-	auto_animate = true
+func start(input: bool = true) -> void:
+	auto_animate = input
 
 
 func finish() -> void:
